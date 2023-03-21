51ec4216af5a8ca27692683b2c104f343904ac5c
leobenedictus <leobenedictus@gmail.com>
2023-03-21 17:32:41 +0000

    tries to tidy copy

--- test3.py
+++ test3.py
@@ -14,10 +14,13 @@ soup = BeautifulSoup(webpage, "html.parser")
 headline = soup.find("h1").get_text()
 standfirst = soup.find("h2").get_text()
 copy_mess = soup.find_all("p")
+
+copy = "".join(p for p in copy_mess)
+
 # copy = [line.get_text() for line in copy_mess if line.attrs["class"][0] == 'ssrcss-1q0x1qg-Paragraph' and line.parent.attrs["class"] != ("ssrcss-1f3bvyz-Stack e1y4nx260" or "ssrcss-y7krbn-Stack e1y4nx260")]
-last_updated_time = soup.find(attrs={'property':'og:updated_time'})
-article = copy_mess, last_updated_time
+# last_updated_time = soup.find(attrs={'property':'og:updated_time'})
+article = copy_mess
 
 st.title(headline)
 st.header(standfirst)
-st.markdown(article) 
+st.markdown(copy) 
